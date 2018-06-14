6d06d7f2f89bc7b7c1a8fb8bc8e504305a16932d
R Sam Abisherk <32828472+samabisherk@users.noreply.github.com>
2018-06-14 11:43:13 +0530

    Add files via upload

---
+++ src/admin/DBconnect.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package admin;
+import mycode.*;
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Sam Abisherk
+ */
+public class DBconnect {
+    
+    
+    public static Connection connect()
+    {
+        Connection conn=null;
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn =(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/projectdb","root","");
+            
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+        
+        return conn;
+    }
+    
+}
